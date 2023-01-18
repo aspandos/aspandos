41e7ceedc7df633fdd6984c648c45fffde1b4a4e
aspandos <98710859+aspandos@users.noreply.github.com>
2023-01-18 22:16:33 +0100

    Create playground.js

---
+++ playground.js
@@ -0,0 +1,65 @@
+console.log('hello')
+console.log('Your mom is gay')
+
+//Variables
+//Description: The variable refers to the value store in it
+//JS variables can hold different data types: numbers, strings,objects and more.
+// Hint: String is just a text 
+name = 'Peter Pan'
+console.log(name)
+
+sentence = 'How are u doing today Peter? I hope you are doing great!'
+console.log(sentence)
+
+//Operators
+//Operators are used to assign values, compare values,perform arithmetic operations
+
+//fruit = prompt('What is your favourite fruit?')
+//console.log(fruit)
+
+food = Number(prompt('How much was the food?'))
+tipPercentage = Number(prompt('tip%?') / 100)
+tipAmount = food * tipPercentage
+
+total = food + tipAmount
+console.log('tip amount',tipAmount)
+console.log('total amount',total)
+//alert9(tipamount)
+
+
+/*Prompt
+The prompt () is a built-in JavaScript functionally that helps you get inputs from a user through the browser.
+
+Data types: any kind of Number,including decimals. (strings and numbers)  
+ Zero or more charachters written inside quoutes.EXAMPLE 
+numbers:1,2,20
+string: 'Hello',
+arrays[]
+objects{}
+boolean true/false
+*/
+
+/*
+Math Operators
+Multiply*
+Divide/
+Exponents **
+Modulo/Remainder % 5%2=1
+Add +
+Substract - 
+*/
+
+/* Math Methods
+Floor: The Math.floor()method rounds a number down to the nearest integer,and returns the result.
+Floor - Rounds down
+Math.floor (10.50)
+
+Ceil: The Math.Ceil()method rounds a number up to the nearest integer,and returns the result.
+Ceil - Rounds up
+Math.ceil (10.50)
+
+Random: Math.random()returns a random number between 0(inclusive), and 1 (exclusive)
+Random gives a random number
+Math.random()>Random number between 0 and 1
+Math.random()*10>Random number between 0 and 10
+*/
